7bb4c152771fb671ab988eb9340652a0d2c34e07
Artem Titoulenko <artem.titoulenko@gmail.com>
2013-03-14 18:37:33 -0400

    added abstract class for a Piece

---
+++ src/Piece.java
@@ -0,0 +1,26 @@
+
+public abstract class Piece {
+  private int file, rank, color;
+  private String type;
+  
+  public String getColor() {
+    return color == 0 ? "b" : "w";
+  }
+  
+  public String getType() {
+    return this.type;
+  }
+  
+  public String getFile() {
+    // lol tricks
+    return (String) (new Object[] {'a', 'b','c','d','e','f','g','h'})[this.file];
+  }
+  
+  public String getRank() {
+    return String.valueOf(this.rank);
+  }
+  
+  public String toString() {
+    return getColor() + getType();
+  }
+}
