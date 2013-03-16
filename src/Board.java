a794645ea05b523a73adeb88338420b6f38e9d1d
Artem Titoulenko <artem.titoulenko@gmail.com>
2013-03-16 02:07:26 -0400

    adding board and board squares

--- src/Board.java
+++ src/Board.java
@@ -1,3 +1,31 @@
 
 public class Board {
+  private BoardSquare[][] squares;
+  
+  public Board() {
+    String color = null;
+    
+    for (int i = 0; i < 8; i++) {
+      color = "white";
+      
+      for (int j = 0; j < 8; j++) {
+        squares[i][j] = new BoardSquare(color);
+        color = (color == "white" ? "black" : "white" );
+      }
+      color = (color == "white" ? "black" : "white" );
+    }
+  }
+  
+  public String toString() {
+    String ret = "";
+    
+    for (int i = 0; i < 8; i++) {
+      for (int j = 0; j < 8; j++) {
+        ret = ret.concat(squares[i][j].toString());
+      }
+      ret = ret.concat("\n");
+    }
+ 
+    return ret;
+  }
 }
--- src/BoardSquare.java
+++ src/BoardSquare.java
@@ -1,16 +1,30 @@
 
 public class BoardSquare {
   private Piece piece;
+  private String color;
   
-  public BoardSquare() {
-    piece = null;
+  public BoardSquare(String color) {
+    this.color = color;
+    this.piece = null;
   }
   
   public boolean isOccupied() {
     return piece == null;
   }
   
+  public Piece getOccupant() {
+    return this.piece;
+  }
+  
   public void occupy(Piece piece) {
     this.piece = piece;
+  }
+  
+  public String getColor() {
+    return this.color;
+  }
+  
+  public String toString() {
+    return this.color;
   }
 }
